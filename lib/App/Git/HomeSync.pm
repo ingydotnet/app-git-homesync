67df4aa2f2130c88adf629d4dddf535abf48873e
Andrew Grangaard <granny-github@ofb.net>
2011-01-20 00:38:12 -0800

    Migrate to Dist::Zilla, it is a monster
    * dist.ini will create LICENSE and README
    * add ABSTRACT to .pm for dist.ini

--- lib/App/Git/HomeSync.pm
+++ lib/App/Git/HomeSync.pm
@@ -2,6 +2,8 @@ package App::Git::HomeSync;
 use Moose;
 use namespace::autoclean;
 
+# ABSTRACT: Sync your home directories via Git
+
 extends qw(MooseX::App::Cmd);
 
 __PACKAGE__->meta->make_immutable;
